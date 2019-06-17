58b1eee80457a52aaf29c11dcf6be93da9300203
Thierry Joux <tjoux@hotmail.com>
2019-06-17 15:00:14 -0700

    MVP Completed

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,46 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  // item.enhancement >= 20? 
+  // {...item, enhancement:20} :
+  // if(item.enhancement < 20)
+  // return {...item, enhancement: item.enhancement + 1}
+  // else 
+  // return {...item, enhancement:20};
+
+  const newItem = item.enhancement < 20 ?
+    {...item, enhancement: item.enhancement + 1} :
+    {...item, enhancement:20};
+
+  return newItem;
 }
 
 function fail(item) {
-  return { ...item };
+
+  const enhancement = item.enhancement;
+  const durability = item.durability
+
+  //Decrement values but can not go lower than 0
+  const durabilityMinus10 = (durability >= 10) ? durability-10 : 0;
+  const durabilityMinus5 = (durability >=5) ? durability-5 : 0;
+
+  //Decrement durability
+  const newDurability = enhancement < 15?
+    durabilityMinus5 : durabilityMinus10;
+
+  //Decrement enhancement
+  const newEnhancement = enhancement > 16?
+    enhancement - 1 : enhancement;
+
+  //return final value
+  return {...item, 
+      enhancement: newEnhancement,
+      durability: newDurability,
+    }
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability:100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,77 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const { repair, succeed, fail } = require('./enhancer.js');
+
+describe('enhancer.js', () => {
+
+  describe('repair()', () => {
+    it('restores durability to 100', () => {
+      expect(repair({ durability: 89 }).durability).toBe(100);
+      expect(repair({ durability: -89 }).durability).toBe(100);
+      expect(repair({ durability: 100 }).durability).toBe(100);
+      expect(repair({ durability: 0 })).toEqual({ durability: 100 });
+    });
+  });
+
+  describe('success()', ()=> {
+      it('increases enhancement by 1', () => {
+          expect(succeed({enhancement:10}).enhancement).toBe(11)
+      })
+
+      it('caps enhancement to 20', () => {
+          expect(succeed({enhancement:20}).enhancement).toBe(20);
+          expect(succeed({enhancement:30}).enhancement).toBe(20);
+      })
+
+      it('checks that durability has not changed', () => {
+          expect(succeed({durability: 50}).durability).toBe(50);
+          expect(succeed({durability: 110}).durability).toBe(110);
+          expect(succeed({durability: 0}).durability).toBe(0);
+          expect(succeed({durability: -50}).durability).toBe(-50);
+      })
+  })
+
+  describe('fail()', () => {
+    it("checks that enhancement decreases by 1 only when it starts with values > 16", () => {
+        expect(fail({enhancement:20}).enhancement).toBe(19);        
+        expect(fail({enhancement:17}).enhancement).toBe(16);
+        expect(fail({enhancement:16}).enhancement).toBe(16);
+        expect(fail({enhancement:0}).enhancement).toBe(0);
+    })
+
+    it('Checks that durability decreases by 5 when enhancement is < 15', () => {
+        expect(fail({enhancement: 14, durability: 50})).toEqual({enhancement:14, durability:45})
+        expect(fail({enhancement: 1, durability: 50})).toEqual({enhancement:1, durability:45})
+
+    })
+    it('Checks that durability decreases by 10 and enhancement is unchanged when enhancement is 15 or 16', 
+        () => {
+        expect(fail({enhancement: 15, durability: 50})).toEqual({enhancement:15, durability:40})
+        expect(fail({enhancement: 16, durability: 10})).toEqual({enhancement:16, durability:0})
+    })
+
+    it('Checks that durablity decreases by 10 and enhancement decreases by 1 when enhancement value > 16',
+        () => {
+        expect(fail({enhancement: 17, durability: 50})).toEqual({enhancement:16, durability:40})
+        expect(fail({enhancement: 20, durability: 10})).toEqual({enhancement:19, durability:0})
+
+    })
+    it('Checks that durablity can  not go lower than 0 when enhancement is < 15',
+        () => {
+        expect(fail({enhancement: 14, durability: 4})).toEqual({enhancement:14, durability:0})
+        expect(fail({enhancement: 1, durability: 0})).toEqual({enhancement:1, durability:0})
+    })
+
+    it('Checks that durablity can  not go lower than 0 when enhancement is >= 15',
+        () => {
+        expect(fail({enhancement: 15, durability: 4})).toEqual({enhancement:15, durability:0})
+        expect(fail({enhancement: 20, durability: 0})).toEqual({enhancement:19, durability:0})
+
+    })
+
+  })
+
+//   describe('success()', () => {});
+
+
+
+});
+
